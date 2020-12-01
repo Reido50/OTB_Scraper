0a538bd660a2e01ec854971e097b9f71a8c14547
Reid Harry <reidoharry50@gmail.com>
2020-11-30 21:22:31 -0600

    Fixed some issues with how the old_disc_list was setup

--- webscrape.py
+++ webscrape.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 import discord_notify as dn
+from time import sleep
 import csv
 import shutil
 import smtplib
@@ -12,53 +13,63 @@ def update():
     num_discs = 20   # Number of products scraped
     my_url = 'https://otbdiscs.com/'    # URL of site to scrap
     notifier = dn.Notifier("https://discordapp.com/api/webhooks/782356542179508254/_VVJxmscOPkztfubKdO3198msha8cjfAEsvntyR76ed7ps3kSucdZlIt20v-TAKy3UsQ")
+    message = ""
 
     # Find directory
     BASE_DIR = os.getcwd()
 
-    # Grab the html from website
-    uClient = urlopen(my_url)
-    page_html = uClient.read()
-    uClient.close()
+    while True:
+        #notifier.send("Loop!", print_message=False)
 
-    # Open old csv file of discs
-    old_discs_csv = open('old_disc_list.csv')
-    csv_reader = csv.reader(old_discs_csv, delimiter=',')
+        # Grab the html from website
+        uClient = urlopen(my_url)
+        page_html = uClient.read()
+        uClient.close()
 
-    # Open new csv file of discs
-    new_discs_csv = open('new_disc_list.csv', "w+")
-    csv_writer = csv.writer(new_discs_csv, delimiter=',')
+        # Open old csv file of discs
+        old_discs_csv = open('old_disc_list.csv')
+        csv_reader = csv.reader(old_discs_csv, delimiter=',')
 
-    # Parse html
-    page_soup = soup(page_html, "html.parser")
+        # Open new csv file of discs
+        new_discs_csv = open('new_disc_list.csv', "w+")
+        csv_writer = csv.writer(new_discs_csv, delimiter=',')
 
-    # Find all products
-    discs = page_soup.findAll("li",{"class":"wc-block-grid__product"})
+        # Parse html
+        page_soup = soup(page_html, "html.parser")
 
-    # Make list
-    old_discs = []
+        # Find all products
+        discs = page_soup.findAll("li",{"class":"wc-block-grid__product"})
 
-    for row in csv_reader:
-        old_discs.append(row[0])
+        # Make list
+        old_discs = []
 
-    count = 0
-    for old_disc in old_discs:
-        # Grab the name
-        name_div = discs[count].findAll("div",{"class":"wc-block-grid__product-title"})
-        disc_name = name_div[0].text
+        for row in csv_reader:
+            old_discs.append(row[0])
 
-        csv_writer.writerow([disc_name])
+        count = 0
+        didItChange = False
+        for old_disc in old_discs:
+            # Grab the name
+            name_div = discs[count].findAll("div",{"class":"wc-block-grid__product-title"})
+            disc_name = name_div[0].text
 
-        if(old_disc != disc_name):
-            didItChange = True
-            if not(disc_name in old_discs):
-                notifier.send("NEW ITEM! " + disc_name, print_message=False)
-                print("NEW DISC!")
-                print(disc_name)
+            csv_writer.writerow([disc_name])
 
-        # Increment the row count
-        count+=1
+            if(old_disc != disc_name):
+                didItChange = True
+                if not(disc_name in old_discs):
+                    message+="NEW ITEM! " + disc_name + "\n"
+                    print("NEW DISC!")
+                    print(disc_name)
 
-    shutil.move(BASE_DIR + "/new_disc_list.csv", BASE_DIR + "/old_disc_list.csv")
+            # Increment the row count
+            count+=1
+
+        if(didItChange):
+            notifier.send(message, print_message=False)
+        shutil.move(BASE_DIR + "/new_disc_list.csv", BASE_DIR + "/old_disc_list.csv")
+
+        base_sleep = 1
+        sleep(base_sleep)
 
 update()
